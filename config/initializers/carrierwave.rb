c8abe3677da86c9d3d9c15193ab7ad0f4ccb7815
Jefferson Jaimes <j.jaimes@uniandes.edu.co>
2016-11-14 12:09:06 -0500

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS', 
-    :aws_access_key_id      => 'AKIAI6C2BGIS5PPLPNKA', 
-    :aws_secret_access_key  => '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', 
+    :aws_access_key_id      => 'AWS_ID', 
+    :aws_secret_access_key  => 'AWS_PASS', 
     :region                 => 'us-west-2'
   }
   config.fog_directory  = 'smartools-videosd'
